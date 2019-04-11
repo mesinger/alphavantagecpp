34aa08bf835263f3a48e6016a2a2ae4c89e6b4ae
mesinger <uintptr96@gmail.com>
2019-04-11 18:07:55 +0200

    low level http req with curl and write callback

--- alphavantagecpp/alphavantage/core/network.hpp
+++ alphavantagecpp/alphavantage/core/network.hpp
@@ -7,48 +7,102 @@
 
 namespace alphavantage {
 	namespace network {
+
+
+
 		namespace https {
 
-			bool GET(const std::string& url);
-			size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata);
+			//forward declarations
 
-			bool GET(const std::string& url) {
+			//write callback function for curl
+			size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
 
-				CURL *curl;
-				CURLcode res;
+			/**
+			class to perform http requests with curl
+			*/
+			class CurlRequest {
+			public:
+				CurlRequest() = default;
+				virtual ~CurlRequest() = default;
 
-				curl_global_init(CURL_GLOBAL_DEFAULT);
-				curl = curl_easy_init();
+				/*
+				performs http request to given url
+				and stores response in buf
+				*/
+				bool send(const std::string& url, char* buf, size_t szbuf) {
 
-				if (curl) {
+					if (!buf || szbuf <= 0) return false;
 
-					curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+					calleeBuffer = buf;
+					szCalleeBuffer = szbuf;
 
-					/*
-					https://curl.haxx.se/libcurl/c/CURLOPT_SSL_VERIFYPEER.html
-					This option determines whether curl verifies the authenticity of the peer's certificate. A value of 1 means curl verifies; 0 (zero) means it doesn't.
-					*/
-					curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+					CURL* curl;
+					CURLcode res;
 
-					curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+					curl_global_init(CURL_GLOBAL_DEFAULT);
+					curl = curl_easy_init();
 
-					res = curl_easy_perform(curl);
+					if (curl) {
 
-					curl_easy_cleanup(curl);
+						curl_easy_setopt(curl, CURLOPT_URL, url.c_str());
+
+						/*
+						https://curl.haxx.se/libcurl/c/CURLOPT_SSL_VERIFYPEER.html
+						This option determines whether curl verifies the authenticity of the peer's certificate. A value of 1 means curl verifies; 0 (zero) means it doesn't.
+						*/
+						curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 1L);
+
+						curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_callback);
+						curl_easy_setopt(curl, CURLOPT_WRITEDATA, this);
+
+						res = curl_easy_perform(curl);
+
+						curl_easy_cleanup(curl);
+					}
+
+					curl_global_cleanup();
+
+					if (res != CURLE_OK)
+						fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+
+					reset();
+
+					return res == CURLE_OK;
 				}
 
-				curl_global_cleanup();
+			protected:
 
-				if (res != CURLE_OK)
-					fprintf(stderr, "curl_easy_perform() failed: %s\n", curl_easy_strerror(res));
+				inline void reset() {
+					calleeBuffer = nullptr;
+					szCalleeBuffer = 0;
+					szReceivedData = 0;
+				}
 
-				std::cout << (res == CURLE_OK);
-				return res == CURLE_OK;
-			}
+			private:
 
-			size_t write_callback(char *ptr, size_t size, size_t nmemb, void *userdata) {
+				//buffer to store response
+				char* calleeBuffer = nullptr;
+				//size of response buffer
+				size_t szCalleeBuffer = 0;
+				//received data
+				size_t szReceivedData = 0;
 
-				
+				friend size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata);
+			};
+
+			size_t write_callback(char* ptr, size_t size, size_t nmemb, void* userdata) {
+
+				CurlRequest* preq = reinterpret_cast<CurlRequest*>(userdata);
+
+				if (preq->szReceivedData + nmemb > preq->szCalleeBuffer) {
+
+					std::cerr << "CurlRequest: response buffer too small" << std::endl;
+					return -1;
+				}
+
+				memcpy(&preq->calleeBuffer[preq->szReceivedData], ptr, nmemb);
+
+				preq->szReceivedData += nmemb;
 
 				return nmemb;
 			}
